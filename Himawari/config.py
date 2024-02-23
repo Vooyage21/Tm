07960c9a814bb2f5c747ead7c37d6d8a353bac2d
Vooyage/MusicBot <148047531+Vooyage21@users.noreply.github.com>
2024-02-24 02:55:40 +0700

    Update config.py

--- Himawari/config.py
+++ Himawari/config.py
@@ -1,26 +1,4 @@
-"""
-MIT License
 
-Copyright (c) 2022 Arsh
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
 
 # Create a new config.py or rename this to config.py file in same dir and
 # import, then extend this class.
@@ -50,9 +28,7 @@ class Config(object):
     API_HASH = ""  # api hash from my.telegram.org
     # mongo database link (necessary)
     MONGO_DB_URL = "mongodb+srv://Xyxx:Helmi4636@cluster0.9yy5ve9.mongodb.net/?retryWrites=true&w=majority"
-    DB_URL = "postgres://thpyouro:Xt9VN1Ybhm9bs8SBk-DU6Jwd6nt_BuGn@bubble.db.elephantsql.com/thpyouro"  # postgres sql database link
-    # redis database url from redislabs.com
-    REDIS_URL = "http://redis-19497.c281.us-east-1-2.ec2.cloud.redislabs.com:19497"
+    DB_URL = "postgres://thpyouro:Xt9VN1Ybhm9bs8SBk-DU6Jwd6nt_BuGn@bubble.db.elephantsql.com/thpyouro"  # postgres sql database link  
     TOKEN = "54dssdV8e59Odo"  # bot token from @BotFather
     DEV_USERS = [940232666]  # developers id
     SUDO_USERS = [940232666]  # sudo users id
